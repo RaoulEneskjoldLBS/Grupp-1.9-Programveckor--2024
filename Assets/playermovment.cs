97ddc67162369b89697f87ab51c0ccb83f595853
RaoulEneskjoldLBS <raoul.eneskjold@elev.ga.lbs.se>
2024-01-09 15:58:50 +0100

    player mevment and script
    svante is skibidi addict

--- Assets/playermovment.cs
+++ Assets/playermovment.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class playermovment : MonoBehaviour
 {
+
+
+    Rigidbody2D playerrigidbody;
+
+
+
     // Start is called before the first frame update
      void Start()
     {
-        
+        playerrigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.D))
+        {
+            playerrigidbody.AddForce(new Vector2(1, 0));
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            playerrigidbody.AddForce(new Vector2(-1, 0));
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            playerrigidbody.AddForce(new Vector2(0, 1));
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            playerrigidbody.AddForce(new Vector2(0, -1));
+        }
     }
 }
