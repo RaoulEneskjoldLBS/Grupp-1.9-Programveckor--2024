c417236b31a8797fae8d5e6936041b7113e469fb
SebastianEklund1 <sebastian.eklund2@elev.ga.lbs.se>
2024-01-12 15:30:45 +0100

    Map

---
+++ Assets/Scripts/Enemy_HP.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_HP : MonoBehaviour
+{
+    public float Enemy_Health_Ammount = 100;
+    public GameObject Enemy;
+    public Rigidbody2D E_rb;
+
+   
+
+
+    void Start()
+    {
+        Enemy_Health_Ammount = 100;
+        E_rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if(collision.gameObject.tag == "Player") //Change "Player" to "Projectile" when projectile object exists
+        {
+            Enemy_Take_Damage(50);
+        }
+    }
+
+    void Update()
+    {
+        
+        if(Enemy_Health_Ammount <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void Enemy_Take_Damage(float Damage)
+    {
+        Enemy_Health_Ammount -= Damage;
+    }
+}
