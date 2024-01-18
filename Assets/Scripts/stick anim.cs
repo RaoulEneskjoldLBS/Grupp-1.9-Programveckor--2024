56f2d2546496e86219dc4bf7793600035b7ea994
RaoulEneskjoldLBS <raoul.eneskjold@elev.ga.lbs.se>
2024-01-18 18:14:11 +0100

    fight stuff

--- Assets/Scripts/stick anim.cs
+++ Assets/Scripts/stick anim.cs
@@ -5,12 +5,25 @@ using UnityEngine;
 public class stickanim : MonoBehaviour
 {
 
-    GameObject stickattackleft;
-    
+
+
+
+
+    public string sticklefttag = "stick left";
+    GameObject stickattackleft = GameObject.FindGameObjectsWithTag(sticklefttag);
+    GameObject stickattackup = GameObject.Find("stickattackup");
+    GameObject stickattackdown = GameObject.Find("stickattackdown");
+    GameObject stickattackright = GameObject.Find("stickattackright");
+
+ 
+
     // Start is called before the first frame update
     void Start()
     {
         stickattackleft = GetComponent<GameObject>();
+        stickattackup = GetComponent<GameObject>();
+        stickattackdown = GetComponent<GameObject>();
+        stickattackright = GetComponent<GameObject>();
     }
 
     // Update is called once per frame
@@ -23,6 +36,36 @@ public class stickanim : MonoBehaviour
         } else
         {
             stickattackleft.SetActive(false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.W) && Input.GetKeyUp(KeyCode.Space))
+        {
+            stickattackup.SetActive(true);
+
+        }
+        else
+        {
+            stickattackup.SetActive(false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.S) && Input.GetKeyUp(KeyCode.Space))
+        {
+            stickattackdown.SetActive(true);
+
+        }
+        else
+        {
+            stickattackdown.SetActive(false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D) && Input.GetKeyUp(KeyCode.Space))
+        {
+            stickattackright.SetActive(true);
+
+        }
+        else
+        {
+            stickattackright.SetActive(false);
         }
     }
 }
